9bb10ac335051cd9e0a6df520bc106c55acafd63
PratyushSingh2002 <nirajdinesh043@gmail.com>
2025-10-22 22:44:28 +0530

    Remove .ipynb_checkpoints folder from tracking

--- .ipynb_checkpoints/03_mydb-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import mysql.connector
-conn=mysql.connector.connect(host="localhost",
-                             user="root",
-                             password="50028812",
-                             database="mydb",)
-df=pd.read_sql("select * from Students",conn)
-print(df)
